87a45932507a86cae421044bbaad47fd384beae0
Viorel Strogoteanu <viorelss@icloud.com>
2022-04-11 16:08:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const numero = 3;
+
+if (numero > 2) {
+    console.log("il numero è maggiore di 2");
+} else if (numero == 2) {
+    console.log("il numero è 2");
+} else {
+    console.log("il numero è minore di 2");
+}
+
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
