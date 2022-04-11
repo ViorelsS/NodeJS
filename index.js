e999056d9056e50210dd677a104391bda8ae6b80
Viorel Strogoteanu <viorelss@icloud.com>
2022-04-11 16:14:46 +0200

    [Globali] test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const numero = 3;
+/*
+__dirname: percorso alla cartella corrente
+__filename: nome del file
+require: funzione per usare i moduli
+module: info sul modulo corrente
+process: info relative all'ambiente di esecuzione
+*/
 
-if (numero > 2) {
-    console.log("il numero è maggiore di 2");
-} else if (numero == 2) {
-    console.log("il numero è 2");
-} else {
-    console.log("il numero è minore di 2");
-}
-
-for (i = 0; i < 10; i++) {
-    console.log(i);
-}
+console.log("Path della directory : ", __dirname);
+console.log("File che stiamo facendo partire : ", __filename);
+console.log("Require:", require);
+console.log("Modulo : ", module);
+console.log("Process : ", process);
